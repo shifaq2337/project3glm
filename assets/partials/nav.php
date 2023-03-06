15df8dfe74936a907c8851667e48e09a0a05693a
Shifa Quddus <quddusshifa@gmail.com>
2023-03-05 22:09:17 -0500

    add logo to navbar abd index

--- assets/partials/nav.php
+++ assets/partials/nav.php
@@ -1,19 +1,19 @@
 <nav class="navbar fixed-top navbar-expand-lg" style="background-color: #eee2df;">
   <div class="container-fluid">
-    <a class="navbar-brand" href="../index.php">GLM</a>
+    <a class="navbar-brand" href="../index.php"><img src="assets/imgs/glmlogo.png"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
       <ul class="navbar-nav mx-auto me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="../index.php"><h4>Home</h4></a>
+          <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../index.php"><h4>About</h4></a>
+          <a class="nav-link" href="../index.php">About</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../list.php"><h4>View Grocery List</h4></a>
+          <a class="nav-link" href="../list.php">View Grocery List</a>
         </li>
       </ul>
     </div>
--- index.php
+++ index.php
@@ -16,7 +16,14 @@
     <?php require_once('assets/partials/nav.php'); ?>
     <div class="container mt-5 pt-5" id="aboutp">
         <h2>About Grocery List Maker(GLM)</h2>
-        <p id="aboutpara" class="rounded p-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div>
+            <div class="text-center">
+                <img src="assets/imgs/glmlogo.png">
+            </div>
+            <div>
+                <p id="aboutpara" class="rounded p-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
     </div>
     <div class="container mt-5">
         <h1>Grocery List Maker</h1>
